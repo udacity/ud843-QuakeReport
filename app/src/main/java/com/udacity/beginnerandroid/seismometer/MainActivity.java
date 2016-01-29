71c74dad54b8aee06a839835d84f769997dfd106
Chris Lei <chris.lei@udacity.com>
2016-01-28 17:13:18 -0800

    First Version Of Settings UI In Place, Not Connected to Networking Core Yet

--- app/src/main/java/com/udacity/beginnerandroid/seismometer/MainActivity.java
+++ app/src/main/java/com/udacity/beginnerandroid/seismometer/MainActivity.java
@@ -1,12 +1,15 @@
 package com.udacity.beginnerandroid.seismometer;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -14,6 +17,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.udacity.beginnerandroid.seismometer.Model.Feature;
+import com.udacity.beginnerandroid.seismometer.Settings.SettingsActivity;
 import com.udacity.beginnerandroid.seismometer.Util.ParsingUtils;
 
 import org.json.JSONArray;
@@ -62,6 +66,25 @@ public class MainActivity extends AppCompatActivity {
             String baseUrl = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2016-01-13&endtime=2016-01-20&limit=20&orderby=magnitude";
             new FetchEarthquakeDataTask().execute(baseUrl);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private class FetchEarthquakeDataTask extends AsyncTask<String, Void, String> {
---
+++ app/src/main/java/com/udacity/beginnerandroid/seismometer/Settings/SettingsActivity.java
@@ -0,0 +1,92 @@
+package com.udacity.beginnerandroid.seismometer.Settings;
+
+import android.app.FragmentTransaction;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.app.FragmentManager;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.udacity.beginnerandroid.seismometer.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+
+        // Display the settings fragment as the main content
+
+        /* Without Method Chaining - Might be better for Beginners
+        FragmentManager mFragmentManager = getFragmentManager();
+        FragmentTransaction mFragmentTransaction = mFragmentManager.beginTransaction();
+        SettingsFragment mSettingsFragment = new SettingsFragment();
+        mFragmentTransaction.replace(android.R.id.content, mSettingsFragment);
+        mFragmentTransaction.commit();
+        */
+
+        // Method Chaining Approach Often Seen Through The Android Framework
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
+
+    // TODO: Explain to students why this declaration is static
+    public static class SettingsFragment extends PreferenceFragment
+            implements Preference.OnPreferenceChangeListener {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the settings_main from an XML resource
+            addPreferencesFromResource(R.xml.settings_main);
+
+            // For all preferences, attach an OnPreferenceChangeListener so the UI summary can be
+            // updated when the preference changes.
+            bindPreferenceSummaryToValue(findPreference(
+                    getString(R.string.list_preference_region_key)));
+            bindPreferenceSummaryToValue(findPreference(
+                    getString(R.string.list_preference_sort_by_key)));
+
+        }
+
+        /**
+         * Attaches a listener so the summary is always updated with the preference value.
+         * Also fires the listener once, to initialize the summary (so it shows up before the value
+         * is changed.)
+         */
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            // Set the listener to watch for value changes.
+            preference.setOnPreferenceChangeListener(this);
+
+            // Trigger the listener immediately with the preference's
+            // current value.
+            onPreferenceChange(preference,
+                    PreferenceManager
+                            .getDefaultSharedPreferences(preference.getContext())
+                            .getString(preference.getKey(), ""));
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
+
+            if (preference instanceof ListPreference) {
+                // For list preferences, look up the correct display value in
+                // the preference's 'entries' list (since they have separate labels/values).
+                ListPreference listPreference = (ListPreference) preference;
+                int prefIndex = listPreference.findIndexOfValue(stringValue);
+                if (prefIndex >= 0) {
+                    preference.setSummary(listPreference.getEntries()[prefIndex]);
+                }
+            } else {
+                // For other preferences, set the summary to the value's simple string representation.
+                preference.setSummary(stringValue);
+            }
+            return true;
+        }
+    }
+}
