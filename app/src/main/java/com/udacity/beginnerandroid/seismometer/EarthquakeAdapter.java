1cc560fae52e81e99f808fb0f28d7292901223ce
Jeremy Silver <jeremy@udacity.com>
2016-03-02 10:57:04 -0800

    Clean up adapter

--- app/src/main/java/com/udacity/beginnerandroid/seismometer/EarthquakeAdapter.java
+++ app/src/main/java/com/udacity/beginnerandroid/seismometer/EarthquakeAdapter.java
@@ -15,14 +15,13 @@ import java.util.ArrayList;
 import java.util.Locale;
 
 /**
- * Let's build an adaptor, to get our data from our Array of earthquakes into the list view in our activity!
+ * Let's build an adaptor, to get our data from our Array of earthquakes into the list view in our
+ * activity!
  */
 public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
 
-    // TODO: Move click listener back here
-    // TODO: Should these be final?
-    private Context mContext;
-    private LayoutInflater mInflater;
+    private final Context mContext;
+    private final LayoutInflater mInflater;
 
     public EarthquakeAdapter(Context context, LayoutInflater inflater, ArrayList<Earthquake> earthquakes) {
         super(context, R.layout.earthquake_list_item, earthquakes);
@@ -32,33 +31,37 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        View result = convertView;
-        if (result == null) {
-            result = mInflater.inflate(R.layout.earthquake_list_item, parent, false);
+        View view = convertView;
+        if (view == null) {
+            view = mInflater.inflate(R.layout.earthquake_list_item, parent, false);
         }
 
         // Try to get view cache or create a new one if needed
-        ViewCache viewCache = (ViewCache) result.getTag();
-        if (viewCache == null) {
-            viewCache = new ViewCache(result);
-            result.setTag(viewCache);
+        ViewHolder viewHolder = (ViewHolder) view.getTag();
+        if (viewHolder == null) {
+            viewHolder = new ViewHolder(view);
+            view.setTag(viewHolder);
         }
 
         // Fetch item
-        final Earthquake earthquakeFeature = getItem(position);
+        Earthquake earthquake = getItem(position);
 
         // Bind the data
-        double magnitude = earthquakeFeature.getMagnitude();
-        viewCache.magnitudeView.setText(String.format(Locale.US, "%.1f", magnitude));
+        double magnitude = earthquake.getMagnitude();
+        String roundedMagnitude = String.format(Locale.US, "%.1f", magnitude);
+        viewHolder.magnitudeView.setText(roundedMagnitude);
+        viewHolder.locationView.setText(earthquake.getLocation());
+        viewHolder.locationDetailsView.setText(earthquake.getLocationDetails());
+        viewHolder.dateView.setText(earthquake.getDate());
+        viewHolder.timeView.setText(earthquake.getTime());
 
         // Adjust the color of the circle background depending on the strength of quake
-        GradientDrawable magnitudeCircle = (GradientDrawable) viewCache.magnitudeView.
+        GradientDrawable magnitudeCircle = (GradientDrawable) viewHolder.magnitudeView.
                 getBackground();
-
 
         int magnitudeColorId;
 
-        switch((int) Math.floor(magnitude)) {
+        switch ((int) Math.floor(magnitude)) {
             case 0:
             case 1:
                 magnitudeColorId = R.color.magnitude1;
@@ -95,38 +98,21 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         final int magnitudeColor = ContextCompat.getColor(mContext, magnitudeColorId);
         magnitudeCircle.setColor(magnitudeColor);
 
-        viewCache.locationView.setText(earthquakeFeature.getLocation());
-        viewCache.locationDetailsView.setText(earthquakeFeature.getLocationDetails());
-        viewCache.dateView.setText(earthquakeFeature.getDate());
-        viewCache.timeView.setText(earthquakeFeature.getTime());
 
-        return result;
+
+        return view;
     }
 
-    /**
-     *
-     * Cache of views in the grid item view to make recycling of views quicker. This avoids additional
-     * {@link View#findViewById(int)} calls after the {@link ViewCache} is first created for a view. See
-     * {@link EarthquakeAdapter#getView(int position, View convertView, ViewGroup parent)}.
-     */
-    public static class ViewCache {
-        /**
-         * View that displays the default magnitude of the Earthquake
-         */
+
+    class ViewHolder {
+
         public final TextView magnitudeView;
-
-
         public final TextView locationView;
         public final TextView locationDetailsView;
         public final TextView dateView;
         public final TextView timeView;
 
-        /**
-         * Constructs a new {@link ViewCache}.
-         *
-         * @param view which contains children views that should be cached.
-         */
-        public ViewCache(View view) {
+        public ViewHolder(View view) {
             magnitudeView = (TextView) view.findViewById(R.id.feature_magnitude);
             locationView = (TextView) view.findViewById(R.id.feature_location);
             locationDetailsView = (TextView) view.findViewById(R.id.feature_location_details);
