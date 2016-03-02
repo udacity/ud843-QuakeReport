f8aa5f7192d35b5e1bffdf9e36b501ed755579ca
Jeremy Silver <jeremy@udacity.com>
2016-03-02 09:37:42 -0800

    Move ViewCache into EarthquakeAdapter

--- app/src/main/java/com/udacity/beginnerandroid/seismometer/EarthquakeAdapter.java
+++ app/src/main/java/com/udacity/beginnerandroid/seismometer/EarthquakeAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import com.udacity.beginnerandroid.seismometer.Model.Earthquake;
 
@@ -102,4 +103,35 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         return result;
     }
 
+    /**
+     *
+     * Cache of views in the grid item view to make recycling of views quicker. This avoids additional
+     * {@link View#findViewById(int)} calls after the {@link ViewCache} is first created for a view. See
+     * {@link EarthquakeAdapter#getView(int position, View convertView, ViewGroup parent)}.
+     */
+    public static class ViewCache {
+        /**
+         * View that displays the default magnitude of the Earthquake
+         */
+        public final TextView magnitudeView;
+
+
+        public final TextView locationView;
+        public final TextView locationDetailsView;
+        public final TextView dateView;
+        public final TextView timeView;
+
+        /**
+         * Constructs a new {@link ViewCache}.
+         *
+         * @param view which contains children views that should be cached.
+         */
+        public ViewCache(View view) {
+            magnitudeView = (TextView) view.findViewById(R.id.feature_magnitude);
+            locationView = (TextView) view.findViewById(R.id.feature_location);
+            locationDetailsView = (TextView) view.findViewById(R.id.feature_location_details);
+            dateView = (TextView) view.findViewById(R.id.feature_date);
+            timeView = (TextView) view.findViewById(R.id.feature_time);
+        }
+    }
 }
