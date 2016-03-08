04a0ecacb06a261edb32af7dff7c6468d36918cb
Jeremy Silver <jeremy@udacity.com>
2016-03-08 12:44:56 -0800

    Extract URL building into its own method

--- app/src/main/java/com/udacity/beginnerandroid/seismometer/EarthquakeListActivity.java
+++ app/src/main/java/com/udacity/beginnerandroid/seismometer/EarthquakeListActivity.java
@@ -54,15 +54,14 @@ public class EarthquakeListActivity extends AppCompatActivity {
         mProgressBar = (ProgressBar) findViewById(R.id.loading_indicator);
         mErrorTextView = (TextView) findViewById(R.id.error_text_view);
 
-        // initialize Earthquake Array
         mEarthquakeList = new ArrayList<>();
 
-        // Fetch the {@link LayoutInflater} service so that new views can be created
-        LayoutInflater inflater = (LayoutInflater) getSystemService(
-                Context.LAYOUT_INFLATER_SERVICE);
+        mConnectionManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mEarthquakeAdapter = new EarthquakeAdapter(this, inflater, mEarthquakeList);
 
         ListView list = (ListView) findViewById(R.id.quake_list_view);
-        mEarthquakeAdapter = new EarthquakeAdapter(this, inflater, mEarthquakeList);
         list.setAdapter(mEarthquakeAdapter);
 
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -75,9 +74,6 @@ public class EarthquakeListActivity extends AppCompatActivity {
             }
         });
 
-        mConnectionManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-
-        // Initialize a Few Locations That Users Can Choose From
         // TODO Add More Cities To Cover More Of The Globe, Preferably with Tectonic Activity
         mRegionsMap = new HashMap<>();
         mRegionsMap.put(
@@ -99,14 +95,11 @@ public class EarthquakeListActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here.
         int id = item.getItemId();
-
         if (id == R.id.action_settings) {
             startActivity(new Intent(this, EarthquakeListSettingsActivity.class));
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -120,16 +113,6 @@ public class EarthquakeListActivity extends AppCompatActivity {
         NetworkInfo networkInfo = mConnectionManager.getActiveNetworkInfo();
         if (networkInfo != null && networkInfo.isConnected()) {
 
-            // Construct the URL for the USGS API query
-            // Possible parameters are available at The USGS Earthquake API page, at
-            // http://earthquake.usgs.gov/fdsnws/event/1/#parameters
-
-
-            // Parameters specific to Circular Search/Using a Radius in Kilometers
-            // http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&latitude=35.6833&longitude=139.6833&maxradiuskm=80&limit=20&orderby=magnitude&eventtype=earthquake
-
-
-            // Extract Configuration From SharedPreferences
             SharedPreferences sharedPrefs =
                     PreferenceManager.getDefaultSharedPreferences(this);
 
@@ -150,35 +133,9 @@ public class EarthquakeListActivity extends AppCompatActivity {
                     getString(R.string.settings_max_radius_key),
                     getString(R.string.settings_max_radius_default));
 
-            Builder uriBuilder = Uri.parse(getString(R.string.base_url)).buildUpon()
-                    .appendQueryParameter(getString(R.string.format_param), getString(R.string.response_format))
-                    .appendQueryParameter(getString(R.string.limit_param), getString(R.string.limit))
-                    .appendQueryParameter(getString(R.string.event_type_param), getString(R.string.event_type))
-                    .appendQueryParameter(getString(R.string.order_by_param), orderBy)
-                    .appendQueryParameter(getString(R.string.min_magnitude_param), minMagnitude);
 
-            if (!region.equals(getString(R.string.settings_region_default))) {
-                String latitude = Double.toString(mRegionsMap.get(region).mLatitude);
-                String longitude = Double.toString(mRegionsMap.get(region).mLongitude);
-
-                uriBuilder.appendQueryParameter(getString(R.string.latitude_param), latitude)
-                        .appendQueryParameter(getString(R.string.longitude_param), longitude)
-                        .appendQueryParameter(getString(R.string.max_radius_param), maxRadius);
-            }
-
-
-            try {
-                URL url = new URL(uriBuilder.toString());
-                new FetchEarthquakeDataTask().execute(url);
-            } catch (MalformedURLException e) {
-                Log.e(LOG_TAG, "Problem building the URL: " + e.getLocalizedMessage());
-            }
-
-            // For Debug
-            Log.d(LOG_TAG, "URL BUILT is " + uriBuilder.toString());
-
-            // TODO: Start Teaching Students Using A Base URL String
-            //String baseUrl = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2016-01-21&endtime=2016-01-28&limit=20&orderby=magnitude&eventtype=earthquake";
+            URL url = buildURL(region, orderBy, minMagnitude, maxRadius);
+            new FetchEarthquakeDataTask().execute(url);
 
         } else {
             mErrorTextView.setText(getString(R.string.error_no_connection));
@@ -186,13 +143,37 @@ public class EarthquakeListActivity extends AppCompatActivity {
 
     }
 
-    // TODO: Extract into
+    private URL buildURL(String region, String orderBy, String minMagnitude, String maxRadius) {
+        Builder uriBuilder = Uri.parse(getString(R.string.base_url)).buildUpon()
+                .appendQueryParameter(getString(R.string.format_param), getString(R.string.response_format))
+                .appendQueryParameter(getString(R.string.limit_param), getString(R.string.limit))
+                .appendQueryParameter(getString(R.string.event_type_param), getString(R.string.event_type))
+                .appendQueryParameter(getString(R.string.order_by_param), orderBy)
+                .appendQueryParameter(getString(R.string.min_magnitude_param), minMagnitude);
+
+        if (!region.equals(getString(R.string.settings_region_default))) {
+            String latitude = Double.toString(mRegionsMap.get(region).mLatitude);
+            String longitude = Double.toString(mRegionsMap.get(region).mLongitude);
+
+            uriBuilder.appendQueryParameter(getString(R.string.latitude_param), latitude)
+                    .appendQueryParameter(getString(R.string.longitude_param), longitude)
+                    .appendQueryParameter(getString(R.string.max_radius_param), maxRadius);
+        }
+
+        URL url = null;
+        try {
+            url = new URL(uriBuilder.toString());
+        } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "Problem building the URL: " + e.getLocalizedMessage());
+        }
+        return url;
+    }
+
+
     private class FetchEarthquakeDataTask extends AsyncTask<URL, Void, ArrayList<Earthquake>> {
 
         private static final long LOADING_INDICATOR_DELAY = 500L;
-
         final Handler handler = new Handler();
-
         final Runnable displayLoadingIndicator = new Runnable() {
             @Override
             public void run() {
@@ -214,20 +195,13 @@ public class EarthquakeListActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(ArrayList<Earthquake> result) {
             if (result != null && !result.isEmpty()) {
-                mEarthquakeList = result;
-
                 mEarthquakeAdapter.clear();
-                mEarthquakeAdapter.addAll(mEarthquakeList);
-
+                mEarthquakeAdapter.addAll(result);
             } else {
                 mErrorTextView.setText(getString(R.string.error_no_results));
             }
-
             handler.removeCallbacks(displayLoadingIndicator);
             mProgressBar.setVisibility(View.INVISIBLE);
         }
     }
 }
-
-
-
