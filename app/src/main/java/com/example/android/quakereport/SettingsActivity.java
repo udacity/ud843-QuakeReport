87abd6b69a67b620f599a1286102a55b1df1c820
Jeremy Silver <jeremy@udacity.com>
2016-07-20 11:22:37 -0700

    1.15 Add min magnitude preference

--- app/src/main/java/com/example/android/quakereport/SettingsActivity.java
+++ app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -29,5 +29,10 @@ public class SettingsActivity extends AppCompatActivity {
 
     public static class EarthquakePreferenceFragment extends PreferenceFragment {
 
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_main);
+        }
     }
 }
