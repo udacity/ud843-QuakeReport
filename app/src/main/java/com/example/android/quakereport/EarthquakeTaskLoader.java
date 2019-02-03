491471c1ccb31aff128c2b7009ee0daf86230957
KUNAL FARMAH <kunalfarmah98@gmail.com>
2019-02-03 20:31:55 +0530

    final app

--- app/src/main/java/com/example/android/quakereport/EarthquakeTaskLoader.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeTaskLoader.java
@@ -4,13 +4,17 @@ import android.content.AsyncTaskLoader;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.View;
 
-import com.example.android.quakereport.Earthquake;
-import com.example.android.quakereport.QueryUtils;
 
 import java.util.ArrayList;
 
+import static com.example.android.quakereport.Earthquake_LoaderActivity.LOG_TAG;
+
 public class EarthquakeTaskLoader extends AsyncTaskLoader<ArrayList<Earthquake>> {
+
+
 
     // getting the url for api calls
     String murl;
@@ -23,6 +27,7 @@ public class EarthquakeTaskLoader extends AsyncTaskLoader<ArrayList<Earthquake>>
 
     @Override
     protected void onStartLoading() {
+        Log.e(LOG_TAG,"Loading");
         // to make android start doing background tasks
         forceLoad();
     }
@@ -36,7 +41,13 @@ public class EarthquakeTaskLoader extends AsyncTaskLoader<ArrayList<Earthquake>>
             return null;
         }
 
+//        Earthquake_LoaderActivity.load.setVisibility(View.VISIBLE);
+//        Earthquake_LoaderActivity.pb.setVisibility(View.VISIBLE);
+
+        Log.e(LOG_TAG,"Fetching data");
         // Don't perform the request if there are no URLs, or the first URL is null.
         return QueryUtils.fetchEarthquakeData(murl);
+
+
     }
 }
--- app/src/main/java/com/example/android/quakereport/Earthquake_LoaderActivity.java
+++ app/src/main/java/com/example/android/quakereport/Earthquake_LoaderActivity.java
@@ -16,11 +16,19 @@
 package com.example.android.quakereport;
 
 import android.app.LoaderManager;
+import android.content.Context;
 import android.content.Loader;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import java.net.InetAddress;
 import java.util.ArrayList;
 
 public class Earthquake_LoaderActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<ArrayList<Earthquake>> {
@@ -28,9 +36,16 @@ public class Earthquake_LoaderActivity extends AppCompatActivity implements Load
     private static final String USGS_REQUEST_URL= "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=4&limit=100";
     private static final int EARTHQUAKE_LOADER_ID = 1;
 
+    static TextView load ;
+    static ProgressBar pb;
+    Button retry;
+
+    TextView empty_State_tv;
+
     public static final String LOG_TAG = EarthquakeAsyncTaskActivity.class.getName();
 
     earthquake_arrayAdpater mAdapter;
+    ListView earthquakeListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +53,32 @@ public class Earthquake_LoaderActivity extends AppCompatActivity implements Load
         setContentView(R.layout.earthquake_activity);
 
 
+
+        empty_State_tv = findViewById(R.id.empty_state);
+        load=findViewById(R.id.loading);
+        pb = findViewById(R.id.progress);
+        retry = findViewById(R.id.retry);
+
+
+
+        if(!isNetworkConnected()){
+            load.setVisibility(View.GONE);
+            pb.setVisibility(View.GONE);
+            empty_State_tv.setText("No Internet Connection Available");
+            //retry.setVisibility(View.VISIBLE);
+        }
+//        else
+//            retry.setVisibility(View.GONE);
+
+//        if(isNetworkConnected() && !isInternetAvailable()){
+//            empty_State_tv.setText("Connection has no Internet Access");
+//        }
+
+
         // Find a reference to the {@link ListView} in the layout
-        ListView earthquakeListView = (ListView) findViewById(R.id.list);
+        earthquakeListView= (ListView) findViewById(R.id.list);
+        earthquakeListView.setEmptyView(empty_State_tv);
+
 
         // Create a new adapter that takes an empty list of earthquakes as input
         mAdapter = new earthquake_arrayAdpater(this, new ArrayList<Earthquake>());
@@ -51,11 +90,21 @@ public class Earthquake_LoaderActivity extends AppCompatActivity implements Load
         // so the list can be populated in the user interface
         earthquakeListView.setAdapter(mAdapter);
 
+//        retry.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                getLoaderManager().initLoader(EARTHQUAKE_LOADER_ID,null,
+//                        Earthquake_LoaderActivity.this);
+//            }
+//        });
+
 
     }
 
     @Override
     public Loader<ArrayList<Earthquake>> onCreateLoader(int i, Bundle bundle) {
+
+        Log.e(LOG_TAG,"Created Loader");
         // create and return a loader with the required url for api calls
         return new EarthquakeTaskLoader(this,USGS_REQUEST_URL);
     }
@@ -64,19 +113,52 @@ public class Earthquake_LoaderActivity extends AppCompatActivity implements Load
     public void onLoadFinished(Loader<ArrayList<Earthquake>> loader, ArrayList<Earthquake> earthquakes) {
         // Clear the adapter of previous earthquake data
         mAdapter.clear();
+        Log.e(LOG_TAG,"Fetched data");
+
+        load.setVisibility(View.GONE);
+        pb.setVisibility(View.GONE);
 
         // If there is a valid list of {@link Earthquake}s, then add them to the adapter's
         // data set. This will trigger the ListView to update.
         if (earthquakes != null && !earthquakes.isEmpty()) {
             mAdapter.addAll(earthquakes);
         }
+
+        else if (!isNetworkConnected()){
+            empty_State_tv.setText("No Internet Connection Available");
+        }
+
+        else if (isNetworkConnected() && earthquakes == null && earthquakes.isEmpty()){
+            empty_State_tv.setText("No Earthquakes Found !!");
+        }
+
     }
+
 
     @Override
     public void onLoaderReset(Loader<ArrayList<Earthquake>> loader) {
+        Log.e(LOG_TAG,"Reset Loader");
 
         // Loader reset, so we can clear out our existing data.
         mAdapter.clear();
+    }
+
+
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return cm.getActiveNetworkInfo() != null;
+    }
+
+    public boolean isInternetAvailable() {
+        try {
+            InetAddress ipAddr = InetAddress.getByName("google.com");
+            //You can replace it with your name
+            return !ipAddr.equals("");
+
+        } catch (Exception e) {
+            return false;
+        }
     }
 
 
