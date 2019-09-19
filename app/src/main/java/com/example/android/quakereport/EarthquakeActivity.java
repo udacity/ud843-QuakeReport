7cb0c8e1ed8dc6cc465acb1a9c34d28c0571130f
nikhilsutar123 <nkhlstr@gmail.com>
2019-09-19 20:25:26 +0530

    update EarthquakeActivity.java

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -15,41 +15,129 @@
  */
 package com.example.android.quakereport;
 
+import android.app.LoaderManager;
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+import android.content.Intent;
+import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
-public class EarthquakeActivity extends AppCompatActivity {
+public class EarthquakeActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Earthquake>> {
 
-    public static final String LOG_TAG = EarthquakeActivity.class.getName();
+    private static final String USGS = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=5&limit=10";
+    ListView earthquakeListView;
+    TextView empty;
+    ProgressBar spinner;
+    /**
+     * Adapter for the list of earthquakes
+     */
+    private MyEQadapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.earthquake_activity);
+        setContentView(R.layout.activity_main);
 
-        // Create a fake list of earthquake locations.
-        ArrayList<String> earthquakes = new ArrayList<>();
-        earthquakes.add("San Francisco");
-        earthquakes.add("London");
-        earthquakes.add("Tokyo");
-        earthquakes.add("Mexico City");
-        earthquakes.add("Moscow");
-        earthquakes.add("Rio de Janeiro");
-        earthquakes.add("Paris");
+        empty = findViewById(R.id.empty);
 
         // Find a reference to the {@link ListView} in the layout
-        ListView earthquakeListView = (ListView) findViewById(R.id.list);
+        earthquakeListView = findViewById(R.id.list);
+        spinner = findViewById(R.id.loading_spinner);
+        spinner.setVisibility(View.VISIBLE);
+
+        /**
+         * Get the network info
+         * @param context
+         */
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo network = cm != null ? cm.getActiveNetworkInfo() : null;
+
+        if (network != null && network.isConnectedOrConnecting()) {
+            //If there is a connection, initialize the Loader
+            Log.i("EarthquakeActivity", "onCreate() started & Loader is Force-initialized!");
+            getLoaderManager().initLoader(1, null, this).forceLoad();
+        } else {
+            empty.setText(R.string.NoConnection);
+            earthquakeListView.setEmptyView(empty);
+        }
 
         // Create a new {@link ArrayAdapter} of earthquakes
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                this, android.R.layout.simple_list_item_1, earthquakes);
+        mAdapter = new MyEQadapter(this, R.layout.earthquake_activity, new LinkedList<Earthquake>());
+
+        earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Earthquake eq = (Earthquake) mAdapter.getItem(i);
+                Intent url = new Intent(Intent.ACTION_VIEW);
+                url.setData(Uri.parse(eq.getUrl()));
+                startActivity(url);
+            }
+        });
 
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
-        earthquakeListView.setAdapter(adapter);
+        earthquakeListView.setAdapter(mAdapter);
+    }
+
+    @Override
+    public Loader<List<Earthquake>> onCreateLoader(int i, Bundle bundle) {
+        Log.i("EarthquakeActivity", "Loader is created");
+        return new EarthQuakeAsyncTaskLoader(EarthquakeActivity.this, USGS);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Earthquake>> loader, List<Earthquake> earthquakes) {
+
+        Log.i("EarthquakeActivity", "spinning stopped because data is displayed on screen");
+        spinner.setVisibility(View.GONE);
+        // Clear the adapter of previous earthquake data
+        mAdapter.clear();
+
+        if (earthquakes != null && !earthquakes.isEmpty())
+            mAdapter.addAll(earthquakes);
+        else
+            empty.setText(R.string.emptyData);
+        earthquakeListView.setEmptyView(empty);
+        Log.i("EarthquakeActivity", "Loader has finished loading. Updating UI");
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<Earthquake>> loader) {
+        // Loader reset, so we can clear out our existing data.
+        mAdapter.clear();
+        Log.i("EarthquakeActivity", "Loader is reset and the activity is closed");
+    }
+
+    public static class EarthQuakeAsyncTaskLoader extends AsyncTaskLoader<List<Earthquake>> {
+        private String mUrl;
+
+        public EarthQuakeAsyncTaskLoader(Context context, String url) {
+            super(context);
+            mUrl = url;
+            Log.i("EarthquakeActivity", "AsyncTaskLoader constructor called");
+        }
+
+        @Override
+        public List<Earthquake> loadInBackground() {
+            // Don't perform the request if there are no URLs, or the first URL is null.
+            if (mUrl == null) {
+                return null;
+            }
+            Log.i("EarthquakeActivity", "data is loading on background thread and fetched");
+            return Queryutils.fetchEarthquakeData(USGS);
+        }
     }
 }
